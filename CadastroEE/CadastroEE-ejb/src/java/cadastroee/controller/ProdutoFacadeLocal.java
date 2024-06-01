48cd7e4c181c05dc261b8a7f3eefd2d4b6ec27e9
Fernandoifs <fernandodesouza.ifs@gmail.com>
2024-06-01 18:05:23 -0300

    adicionado as respostas.

--- CadastroEE/CadastroEE-ejb/src/java/cadastroee/controller/ProdutoFacadeLocal.java
+++ CadastroEE/CadastroEE-ejb/src/java/cadastroee/controller/ProdutoFacadeLocal.java
@@ -8,16 +8,10 @@ import jakarta.ejb.Local;
 public interface ProdutoFacadeLocal {
 
     void create(Produto produto);
-
     void edit(Produto produto);
-
     void remove(Produto produto);
-
     Produto find(Object id);
-
     List<Produto> findAll();
-
     List<Produto> findRange(int[] range);
-
     int count();
 }
